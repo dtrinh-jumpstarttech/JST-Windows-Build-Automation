20b9e92a867e0b030f73111c27e22e69a4829cff
Dan Trinh <147097442+dtrinh-jumpstarttech@users.noreply.github.com>
2024-10-22 10:18:50 -0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Check if running as admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c, %~dpnx0' -Verb runAs"
+    exit /b
+)
+
+:: Set the script directory dynamically (the same location as the .bat file)
+set scriptDir=%~dp0
+
+:: Run the PowerShell script with RemoteSigned execution policy
+powershell -ExecutionPolicy RemoteSigned -File "%scriptDir%master.ps1"
